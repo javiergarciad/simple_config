9ecac5dfd9fb959836047f728ad8ea7cd442e787
Javier Garcia <javier.garcia.d@gmail.com>
2023-10-19 13:13:45 -0500

    Update setup.sh

--- dev_machine/setup.sh
+++ dev_machine/setup.sh
@@ -59,8 +59,8 @@ chsh -s $(which zsh)
 # Install ZSH plugins
 echo "Installing ZSH plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 
 # Replace .zshrc
 echo "Replacing .zshrc..."
